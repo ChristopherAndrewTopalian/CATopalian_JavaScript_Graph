e27ca955f0c89b825efa570b266e708ff7101a93
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-18 15:42:22 -0400

    update

---
+++ src/js/make/makeInfoDiv.js
@@ -0,0 +1,19 @@
+// makeInfoDiv.js
+
+function makeInfoDiv()
+{
+    let infoContainer = ce('textarea');
+    infoContainer.id = 'infoContainer';
+    infoContainer.style.position = 'relative';
+    infoContainer.style.margin = 'auto';
+    ge('mainDiv').after(infoContainer);
+}
+
+//----//
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
